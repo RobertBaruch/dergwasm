f842c04969789934141ed1ac8b5926e667d682b8
Robert Baruch <robert.c.baruch@gmail.com>
2023-11-04 14:34:09 -0700

    Add C#/VS to gitignore, initial mod code

---
+++ dergwasm_mod/Dergwasm/Dergwasm.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Elements.Core;  // For UniLog
+using HarmonyLib;
+using FrooxEngine;
+using ResoniteModLoader;
+using static FrooxEngine.SessionControlDialog;
+
+namespace Derg
+{
+    public class Dergwasm : ResoniteMod
+    {
+        public override string Name => "Dergwasm";
+        public override string Author => "Xekri";
+        public override string Version => "0.1.0";
+
+        public override void OnEngineInit()
+        {
+            Harmony harmony = new Harmony("dev.xekri.Dergwasm");
+            harmony.PatchAll();
+        }
+
+        [HarmonyPatch(typeof(World))]
+        [HarmonyPatch(nameof(World.Load))]
+        class StartRunningPatch
+        {
+            static void Postfix(World __instance)
+            {
+                UniLog.Log("Postfix called on World.Load");
+                UniLog.Log($"... WorldName {__instance.Configuration.WorldName.Value}");
+                UniLog.Log($"... SessionID {__instance.Configuration.SessionID.Value}");
+                Slot dergwasmSlot = __instance.RootSlot.FindChild(s => s.Tag == "_dergwasm", maxDepth: 0);
+                if (dergwasmSlot == null) {
+                    dergwasmSlot = __instance.RootSlot.AddSlot("Dergwasm");
+                    dergwasmSlot.Tag = "_dergwasm";
+                }
+            }
+        }
+    }
+}
