c149820da80d7670c8e8ac586c612acbf00da9c7
Robert Baruch <robert.c.baruch@gmail.com>
2023-11-19 08:55:41 -0800

    More memory insns

--- dergwasm_mod/Dergwasm/MemoryInstructions.cs
+++ dergwasm_mod/Dergwasm/MemoryInstructions.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
 
 namespace Derg
 {
     public static class MemoryInstructions
     {
-        private static unsafe T Convert<T>(byte[] mem, uint ea)
+        public static unsafe T Convert<T>(byte[] mem, uint ea)
             where T : unmanaged
         {
             fixed (byte* ptr = &mem[ea])
@@ -22,12 +24,30 @@ namespace Derg
             }
         }
 
+        private static unsafe void Store<T>(Span<byte> bytes, T value)
+            where T : unmanaged
+        {
+            fixed (byte* ptr = bytes)
+            {
+                *(T*)ptr = value;
+            }
+        }
+
         private static Span<byte> Span0(Instruction instruction, IMachine machine, int sz)
         {
             int offset = machine.Pop().S32;
             // Ignore Operands[0], the alignment.
             int base_addr = instruction.Operands[1].S32;
-            return machine.Span0(base_addr + offset, sz);
+            try
+            {
+                return machine.Span0(base_addr + offset, sz);
+            }
+            catch (Exception e)
+            {
+                throw new Trap(
+                    $"Memory access out of bounds: base {(uint)base_addr:8X} offset {(uint)offset:8X}"
+                );
+            }
         }
 
         public static void I32Load(Instruction instruction, IMachine machine) =>
@@ -71,5 +91,133 @@ namespace Derg
 
         public static void F64Load(Instruction instruction, IMachine machine) =>
             machine.Push(new Value(Convert<double>(Span0(instruction, machine, 8))));
+
+        public static void I32Store(Instruction instruction, IMachine machine)
+        {
+            uint val = machine.Pop().U32;
+            Span<byte> span = Span0(instruction, machine, 4);
+            Store<uint>(span, val);
+        }
+
+        public static void I32Store8(Instruction instruction, IMachine machine)
+        {
+            uint val = machine.Pop().U32;
+            Span0(instruction, machine, 1)[0] = (byte)val;
+        }
+
+        public static void I32Store16(Instruction instruction, IMachine machine)
+        {
+            uint val = machine.Pop().U32;
+            Span<byte> span = Span0(instruction, machine, 2);
+            Store<ushort>(span, (ushort)val);
+        }
+
+        public static void I64Store(Instruction instruction, IMachine machine)
+        {
+            ulong val = machine.Pop().U64;
+            Span<byte> span = Span0(instruction, machine, 8);
+            Store<ulong>(span, val);
+        }
+
+        public static void I64Store8(Instruction instruction, IMachine machine)
+        {
+            ulong val = machine.Pop().U64;
+            Span0(instruction, machine, 1)[0] = (byte)val;
+        }
+
+        public static void I64Store16(Instruction instruction, IMachine machine)
+        {
+            ulong val = machine.Pop().U64;
+            Span<byte> span = Span0(instruction, machine, 2);
+            Store<ushort>(span, (ushort)val);
+        }
+
+        public static void I64Store32(Instruction instruction, IMachine machine)
+        {
+            ulong val = machine.Pop().U64;
+            Span<byte> span = Span0(instruction, machine, 4);
+            Store<uint>(span, (uint)val);
+        }
+
+        public static void MemorySize(Instruction instruction, IMachine machine) =>
+            machine.Push(new Value((uint)machine.Memory0.Length >> 16));
+
+        public static void MemoryGrow(Instruction instruction, IMachine machine)
+        {
+            int memidx = instruction.Operands[0].Int;
+            if (memidx != 0)
+            {
+                throw new Trap("memory.grow: Non-zero memory segment accessed");
+            }
+            uint delta = machine.Pop().U32;
+            Memory mem = machine.GetMemoryFromIndex(0);
+            uint oldSize = (uint)mem.Data.Length >> 16;
+            uint newSize = oldSize + delta;
+            if (mem.Limits.Maximum.HasValue && newSize > mem.Limits.Maximum)
+            {
+                machine.Push(new Value(-1));
+                return;
+            }
+            // .NET has a limitation on value sizes to about 0x80000000 bytes.
+            if (newSize > 0x7FFF)
+            {
+                machine.Push(new Value(-1));
+                return;
+            }
+            Array.Resize(ref mem.Data, (int)newSize << 16);
+            mem.Limits.Minimum = newSize;
+            machine.Push(new Value(oldSize));
+        }
+
+        public static void MemoryFill(Instruction instruction, IMachine machine)
+        {
+            int memidx = instruction.Operands[0].Int;
+            if (memidx != 0)
+            {
+                throw new Trap("memory.fill: Non-zero memory segment accessed");
+            }
+            uint n = machine.Pop().U32;
+            byte val = (byte)machine.Pop().U32;
+            uint d = machine.Pop().U32;
+            Memory mem = machine.GetMemoryFromIndex(0);
+            if (d + n > mem.Data.Length)
+            {
+                throw new Trap(
+                    $"memory.fill: Access out of bounds: offset 0x{d:8X} length 0x{n:8X} bytes"
+                );
+            }
+            // Maddeningly, there's no Array.Fill for .NET 4.7.
+            machine.Span0((int)d, (int)n).Fill(val);
+        }
+
+        public static void MemoryCopy(Instruction instruction, IMachine machine)
+        {
+            int memidx = instruction.Operands[0].Int;
+            if (memidx != 0)
+            {
+                throw new Trap("memory.copy: Non-zero memory segment accessed");
+            }
+            uint n = machine.Pop().U32;
+            uint s = machine.Pop().U32;
+            uint d = machine.Pop().U32;
+            Memory mem = machine.GetMemoryFromIndex(0);
+            if (d + n > mem.Data.Length)
+            {
+                throw new Trap(
+                    $"memory.copy: Destination access out of bounds: offset 0x{d:8X} length 0x{n:8X} bytes"
+                );
+            }
+            if (s + n > mem.Data.Length)
+            {
+                throw new Trap(
+                    $"memory.copy: Source access out of bounds: offset 0x{s:8X} length 0x{n:8X} bytes"
+                );
+            }
+            if (n == 0)
+            {
+                return;
+            }
+            machine.Span0((int)s, (int)n).CopyTo(machine.Span0((int)d, (int)n));
+        }
     }
 }
--- dergwasm_mod/DergwasmTests/MemoryInstructionTests.cs
+++ dergwasm_mod/DergwasmTests/MemoryInstructionTests.cs
@@ -34,6 +34,30 @@ namespace DergwasmTests
         }
 
         [Theory]
+        [InlineData(InstructionType.I32_LOAD, 0xFFFF, 0)]
+        [InlineData(InstructionType.I32_LOAD, 0, 0xFFFF)]
+        [InlineData(InstructionType.I64_LOAD, 0xFFFF, 0)]
+        [InlineData(InstructionType.I64_LOAD, 0, 0xFFFF)]
+        [InlineData(InstructionType.F32_LOAD, 0xFFFF, 0)]
+        [InlineData(InstructionType.F32_LOAD, 0, 0xFFFF)]
+        [InlineData(InstructionType.F64_LOAD, 0xFFFF, 0)]
+        [InlineData(InstructionType.F64_LOAD, 0, 0xFFFF)]
+        public void TestLoadTrapsOnOutOfBounds(InstructionType insn, int offset, int base_addr)
+        {
+            // 0: I32_CONST offset
+            // 1: I32_LOAD _ base_addr
+            // 2: NOP
+            machine.SetProgram(
+                0,
+                I32Const(offset),
+                Insn(insn, new Value(0), new Value(base_addr)),
+                Nop()
+            );
+
+            Assert.Throws<Trap>(() => machine.Step(2));
+        }
+
+        [Theory]
         [InlineData(InstructionType.I64_LOAD, 0, 0, 0x08070605A4A3A2A1UL)]
         [InlineData(InstructionType.I64_LOAD, 0, 1, 0x0908070605A4A3A2UL)]
         [InlineData(InstructionType.I64_LOAD, 1, 0, 0x0908070605A4A3A2UL)]
@@ -106,6 +130,60 @@ namespace DergwasmTests
             machine.Step(2);
 
             Assert.Equal(expected, machine.TopOfStack.F64);
+        }
+
+        [Theory]
+        [InlineData(InstructionType.I32_STORE, 0, 0,   0x1234A6A8U, 0x000000001234A6A8UL)]
+        [InlineData(InstructionType.I32_STORE, 1, 0,   0x1234A6A8U, 0x0000001234A6A800UL)]
+        [InlineData(InstructionType.I32_STORE, 0, 1,   0x1234A6A8U, 0x0000001234A6A800UL)]
+        [InlineData(InstructionType.I32_STORE8, 0, 0,  0xFFFFFFA8U, 0x00000000000000A8UL)]
+        [InlineData(InstructionType.I32_STORE16, 0, 0, 0xFFFFA6A8U, 0x000000000000A6A8UL)]
+        public void TestI32Store(InstructionType insn, int offset, int base_addr, uint val, ulong expected)
+        {
+            // 0: I32_CONST offset
+            // 1: I32_CONST val
+            // 1: I32_STORE _ base_addr
+            // 2: NOP
+            machine.SetProgram(
+                0,
+                I32Const(offset),
+                I32Const(val),
+                Insn(insn, new Value(0), new Value(base_addr)),
+                Nop()
+            );
+
+            machine.Step(3);
+
+            Assert.Empty(machine.Frame.value_stack);
+            Assert.Equal(expected, MemoryInstructions.Convert<ulong>(machine.Memory0, 0));
+        }
+
+        [Theory]
+        [InlineData(InstructionType.I64_STORE, 0, 0,   0xA1A2A3A4A5A6A7A8UL, 0xA1A2A3A4A5A6A7A8UL, 0x00)]
+        [InlineData(InstructionType.I64_STORE, 1, 0,   0xA1A2A3A4A5A6A7A8UL, 0xA2A3A4A5A6A7A800UL, 0xA1)]
+        [InlineData(InstructionType.I64_STORE, 0, 1,   0xA1A2A3A4A5A6A7A8UL, 0xA2A3A4A5A6A7A800UL, 0xA1)]
+        [InlineData(InstructionType.I64_STORE8, 0, 0,  0xFFFFFFFFFFFFFFA1UL, 0x00000000000000A1UL, 0x00)]
+        [InlineData(InstructionType.I64_STORE16, 0, 0, 0xFFFFFFFFFFFFA1A2UL, 0x000000000000A1A2UL, 0x00)]
+        [InlineData(InstructionType.I64_STORE32, 0, 0, 0xFFFFFFFFA1A2A3A4UL, 0x00000000A1A2A3A4UL, 0x00)]
+        public void TestI64Store(InstructionType insn, int offset, int base_addr, ulong val, ulong expected, byte expected8)
+        {
+            // 0: I32_CONST offset
+            // 1: I64_CONST val
+            // 1: I64_STORE _ base_addr
+            // 2: NOP
+            machine.SetProgram(
+                0,
+                I32Const(offset),
+                I64Const(val),
+                Insn(insn, new Value(0), new Value(base_addr)),
+                Nop()
+            );
+
+            machine.Step(3);
+
+            Assert.Empty(machine.Frame.value_stack);
+            Assert.Equal(expected, MemoryInstructions.Convert<ulong>(machine.Memory0, 0));
+            Assert.Equal(expected8, machine.Memory0[8]);
         }
     }
 }
