44d1758d7559b5a95b22e39b97ec2a8af3aab6e2
Robert Baruch <robert.c.baruch@gmail.com>
2023-11-19 14:35:01 -0800

    CLZ, CTZ insns

--- dergwasm_mod/Dergwasm/NumericInstructions.cs
+++ dergwasm_mod/Dergwasm/NumericInstructions.cs
@@ -4,5 +4,64 @@
     {
         public static void Const(Instruction instruction, IMachine machine) =>
             machine.Push(instruction.Operands[0]);
+
+        private static int clz(ulong value, int numbits)
+        {
+            // .Net Framework 4.7.2 doesn't have a built-in clz, so we have to do it ourselves.
+            // There are O(log N) algorithms for this, but it's not worth it -- I don't think
+            // a lot of software is really going to use this instruction.
+            int count = 0;
+            ulong mask = 1UL << (numbits - 1);
+            while (mask != 0 && (value & mask) == 0)
+            {
+                count++;
+                mask >>= 1;
+            }
+            return count;
+        }
+
+        private static int ctz(ulong value, int numbits)
+        {
+            // .Net Framework 4.7.2 doesn't have a built-in ctz, so we have to do it ourselves.
+            // There are O(log N) algorithms for this, but it's not worth it -- I don't think
+            // a lot of software is really going to use this instruction.
+            int count = 0;
+            ulong mask = 1UL;
+            ulong stop_mask = numbits == 64 ? 0 : 1UL << numbits;
+            while (mask != stop_mask && (value & mask) == 0)
+            {
+                count++;
+                mask <<= 1;
+            }
+            return count;
+        }
+
+        // Count leading zeros.
+        public static void I32Clz(Instruction instruction, IMachine machine)
+        {
+            ulong value = machine.Pop().U32;
+            machine.Push(new Value(clz(value, 32)));
+        }
+
+        // Count leading zeros.
+        public static void I64Clz(Instruction instruction, IMachine machine)
+        {
+            ulong value = machine.Pop().U64;
+            machine.Push(new Value(clz(value, 64)));
+        }
+
+        // Count trailing zeros.
+        public static void I32Ctz(Instruction instruction, IMachine machine)
+        {
+            ulong value = machine.Pop().U32;
+            machine.Push(new Value(ctz(value, 32)));
+        }
+
+        // Count trailing zeros.
+        public static void I64Ctz(Instruction instruction, IMachine machine)
+        {
+            ulong value = machine.Pop().U64;
+            machine.Push(new Value(ctz(value, 64)));
+        }
     }
 }
--- dergwasm_mod/DergwasmTests/NumericInstructionTests.cs
+++ dergwasm_mod/DergwasmTests/NumericInstructionTests.cs
@@ -52,5 +52,35 @@ namespace DergwasmTests
             Assert.Equal(1, machine.PC);
             Assert.Collection(machine.Frame.value_stack, e => Assert.Equal(1.1, e.F64));
         }
+
+        [Theory]
+        [InlineData(InstructionType.I32_CLZ, InstructionType.I32_CONST, 0UL, 32UL)]
+        [InlineData(InstructionType.I32_CLZ, InstructionType.I32_CONST, 1UL, 31UL)]
+        [InlineData(InstructionType.I32_CLZ, InstructionType.I32_CONST, 0x80000000UL, 0UL)]
+        [InlineData(InstructionType.I32_CTZ, InstructionType.I32_CONST, 0UL, 32UL)]
+        [InlineData(InstructionType.I32_CTZ, InstructionType.I32_CONST, 1UL, 0UL)]
+        [InlineData(InstructionType.I32_CTZ, InstructionType.I32_CONST, 0x80000000UL, 31UL)]
+        [InlineData(InstructionType.I64_CLZ, InstructionType.I64_CONST, 0UL, 64UL)]
+        [InlineData(InstructionType.I64_CLZ, InstructionType.I64_CONST, 1UL, 63UL)]
+        [InlineData(InstructionType.I64_CLZ, InstructionType.I64_CONST, 0x8000000000000000UL, 0UL)]
+        [InlineData(InstructionType.I64_CTZ, InstructionType.I64_CONST, 0UL, 64UL)]
+        [InlineData(InstructionType.I64_CTZ, InstructionType.I64_CONST, 1UL, 0UL)]
+        [InlineData(InstructionType.I64_CTZ, InstructionType.I64_CONST, 0x8000000000000000UL, 63UL)]
+        public void TestIUnops(
+            InstructionType insn,
+            InstructionType const_insn,
+            ulong input,
+            ulong expected_output
+        )
+        {
+            // 0: const_insn input
+            // 1: insn
+            // 2: NOP
+            machine.SetProgram(0, Insn(const_insn, new Value(input)), Insn(insn), Nop());
+
+            machine.Step(2);
+
+            Assert.Collection(machine.Frame.value_stack, e => Assert.Equal(expected_output, e.U64));
+        }
     }
 }
