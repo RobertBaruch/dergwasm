9ef863c673bc2b76c05366d4ab2a99f31da4a39b
Robert Baruch <robert.c.baruch@gmail.com>
2023-12-25 12:08:58 -0800

    Initial stab at implementing a Resonite API.

--- dergwasm_mod/Dergwasm/ResoniteEnv.cs
+++ dergwasm_mod/Dergwasm/ResoniteEnv.cs
@@ -6,15 +6,83 @@ using System.Text;
 
 namespace Derg
 {
+    // Provides the functions in resonite_api.h. A ResoniteEnv, like a Machine, is specific
+    // to a World.
+    //
+    // FrooxEngine.World doesn't provide any methods to find objects by their ReferenceID.
+    // Therefore, those have to be patched in.
     public class ResoniteEnv
     {
         public Machine machine;
-        public Dictionary<ulong, IWorldElement> worldElements =
-            new Dictionary<ulong, IWorldElement>();
+        public World world;
 
-        public ResoniteEnv(Machine machine)
+        public ResoniteEnv(Machine machine, World world)
         {
             this.machine = machine;
+        }
+
+        public void RegisterHostFuncs()
+        {
+            machine.RegisterReturningHostFunc<ulong, ulong>(
+                "resonite_api", // TODO: Is this right?
+                "slot__get_active_user",
+                slot__get_active_user
+            );
+            machine.RegisterReturningHostFunc<ulong, ulong>(
+                "resonite_api",
+                "slot__get_active_user_root",
+                slot__get_active_user_root
+            );
+            machine.RegisterReturningHostFunc<ulong, int, ulong>(
+                "resonite_api",
+                "slot__get_object_root",
+                slot__get_object_root
+            );
+            machine.RegisterReturningHostFunc<ulong, ulong>(
+                "resonite_api",
+                "slot__get_parent",
+                slot__get_parent
+            );
+        }
+
+        public ulong slot__get_active_user(Frame frame, ulong slot_id)
+        {
+            Slot slot = world.GetSlotByReferenceID(slot_id); // TODO: Implement.
+            if (slot == null)
+            {
+                return 0;
+            }
+            return (ulong)slot.ActiveUser.ReferenceID;
+        }
+
+        public ulong slot__get_active_user_root(Frame frame, ulong slot_id)
+        {
+            Slot slot = world.GetSlotByReferenceID(slot_id);
+            if (slot == null)
+            {
+                return 0;
+            }
+            return (ulong)slot.ActiveUserRoot.ReferenceID;
+        }
+
+        public ulong slot__get_object_root(Frame frame, ulong slot_id, int only_explicit)
+        {
+            Slot slot = world.GetSlotByReferenceID(slot_id);
+            if (slot == null)
+            {
+                return 0;
+            }
+            return (ulong)slot.GetObjectRoot(only_explicit != 0).ReferenceID;
+        }
+
+        public ulong slot__get_parent(Frame frame, ulong slot_id)
+        {
+            Slot slot = world.GetSlotByReferenceID(slot_id);
+            if (slot == null)
+            {
+                return 0;
+            }
+            return (ulong)slot.Parent.ReferenceID;
         }
     }
 }
