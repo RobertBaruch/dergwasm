ef265178e919dec37c3602f5cd65f0fca7970aca
Robert Baruch <robert.c.baruch@gmail.com>
2024-01-19 09:00:08 -0800

    Component Utils and test

---
+++ dergwasm_mod/DergwasmTests/ComponentUtilsTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using BepuPhysics.Constraints;
+using Derg;
+using FrooxEngine;
+using HarmonyLib;
+using Xunit;
+
+namespace DergwasmTests
+{
+    public class TestComponent : Component
+    {
+        public Sync<int> IntField;
+
+        public TestComponent(World world)
+        {
+            IntField = new Sync<int> { Value = 1 };
+            IntField.Initialize(world, null);
+        }
+    }
+
+    public class ComponentUtilsTests
+    {
+        public World world;
+
+        public ComponentUtilsTests()
+        {
+            World world = World.JoinSession(null, new Uri[] { });
+            Assert.NotNull(world.ConnectorManager);
+        }
+
+        [Fact]
+        public void GetSyncValueTest()
+        {
+            var testComponent = new TestComponent(world);
+            FieldInfo fieldInfo = testComponent
+                .GetType()
+                .GetField("Value", BindingFlags.Instance | BindingFlags.Public);
+            fieldInfo.SetValue(testComponent, new Sync<int> { Value = 1 });
+            Assert.Equal(1, ComponentUtils.GetFieldValue(testComponent, "Value"));
+        }
+    }
+}
