7debe5023c76e66fc923daf2206e28248409e41e
Robert Baruch <robert.c.baruch@gmail.com>
2024-01-20 10:54:40 -0800

    Tests for getting component Sync, SyncRef, and properties.

--- dergwasm_mod/DergwasmTests/ComponentUtilsTests.cs
+++ dergwasm_mod/DergwasmTests/ComponentUtilsTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Reflection;
 using Derg;
+using Elements.Core;
 using FrooxEngine;
 using Xunit;
 
@@ -9,11 +10,18 @@ namespace DergwasmTests
     public class TestComponent : Component
     {
         public Sync<int> IntField;
+        public SyncRef<TestComponent> ComponentRefField;
 
-        public TestComponent(World world)
+        public int IntProperty
         {
-            IntField = new Sync<int> { Value = 1 };
-            IntField.Initialize(world, null);
+            get { return IntField.Value; }
+            set { IntField.Value = value; }
+        }
+
+        public TestComponent()
+        {
+            IntField = new Sync<int>();
+            ComponentRefField = new SyncRef<TestComponent>();
         }
     }
 
@@ -27,42 +35,79 @@ namespace DergwasmTests
             //Assert.NotNull(world.ConnectorManager);
         }
 
+        //[Fact]
+        //public void GetSyncValueTest()
+        //{
+        //    var testComponent = new ValueField<int>();
+        //    var intField = new Sync<int>();
+
+        //    FieldInfo fieldInfo = testComponent.GetType().GetField("Value");
+        //    fieldInfo.SetValue(testComponent, intField);
+
+        //    fieldInfo = typeof(Sync<int>).GetField(
+        //        "_value",
+        //        BindingFlags.Instance | BindingFlags.NonPublic
+        //    );
+        //    fieldInfo.SetValue(intField, 1);
+
+        //    Assert.Equal(1, ComponentUtils.GetFieldValue(testComponent, "Value"));
+        //}
+
         [Fact]
         public void GetSyncValueTest()
         {
-            var testComponent = new ValueField<int>();
-            var intField = new Sync<int>();
-
-            FieldInfo fieldInfo = testComponent.GetType().GetField("Value");
-            fieldInfo.SetValue(testComponent, intField);
-
-            fieldInfo = typeof(Sync<int>).GetField(
+            var testComponent = new TestComponent();
+            FieldInfo fieldInfo = typeof(Sync<int>).GetField(
                 "_value",
                 BindingFlags.Instance | BindingFlags.NonPublic
             );
-            fieldInfo.SetValue(intField, 1);
+            fieldInfo.SetValue(testComponent.IntField, 1);
 
-            Assert.Equal(1, ComponentUtils.GetFieldValue(testComponent, "Value"));
+            Assert.Equal(1, ComponentUtils.GetFieldValue(testComponent, "IntField"));
+        }
+
+        [Fact]
+        public void GetSyncRefTest()
+        {
+            var testComponent = new TestComponent();
+            FieldInfo fieldInfo = typeof(SyncRef<TestComponent>).GetField(
+                "_target",
+                BindingFlags.Instance | BindingFlags.NonPublic
+            );
+            fieldInfo.SetValue(testComponent.ComponentRefField, testComponent);
+
+            Assert.Same(
+                testComponent,
+                ComponentUtils.GetFieldValue(testComponent, "ComponentRefField")
+            );
+        }
+
+        [Fact]
+        public void GetPropertyTest()
+        {
+            var testComponent = new TestComponent();
+            FieldInfo fieldInfo = typeof(Sync<int>).GetField(
+                "_value",
+                BindingFlags.Instance | BindingFlags.NonPublic
+            );
+            fieldInfo.SetValue(testComponent.IntField, 1);
+
+            Assert.Equal(1, ComponentUtils.GetFieldValue(testComponent, "IntProperty"));
         }
 
         [Fact]
         public void SetSyncValueTest()
         {
-            var testComponent = new ValueField<int>();
-            var intField = new Sync<int>();
-
-            FieldInfo fieldInfo = testComponent.GetType().GetField("Value");
-            fieldInfo.SetValue(testComponent, intField);
-
-            fieldInfo = typeof(Sync<int>).GetField(
+            var testComponent = new TestComponent();
+            FieldInfo fieldInfo = typeof(Sync<int>).GetField(
                 "_value",
                 BindingFlags.Instance | BindingFlags.NonPublic
             );
-            fieldInfo.SetValue(intField, 1);
+            fieldInfo.SetValue(testComponent.IntField, 1);
 
-            ComponentUtils.SetFieldValue(testComponent, "Value", 12);
+            ComponentUtils.SetFieldValue(testComponent, "IntField", 12);
 
-            Assert.Equal(12, ComponentUtils.GetFieldValue(testComponent, "Value"));
+            Assert.Equal(12, ComponentUtils.GetFieldValue(testComponent, "IntField"));
         }
     }
 }
