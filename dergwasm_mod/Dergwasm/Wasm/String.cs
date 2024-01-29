b4d1c11f87212777e824482adb387746ff4bd044
Robert Baruch <robert.c.baruch@gmail.com>
2024-01-29 13:07:05 -0800

    Adds a NullTerminatedString type

---
+++ dergwasm_mod/Dergwasm/Wasm/String.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Derg.Wasm
+{
+    // Represents a pointer to bytes to be interpreted as a NUL-terminated UTF8-encoded
+    // string.
+    [StructLayout(LayoutKind.Sequential)]
+    public readonly struct NullTerminatedString
+    {
+        public readonly Ptr<byte> Data;
+
+        public NullTerminatedString(Ptr<byte> data)
+        {
+            Data = data;
+        }
+
+        public NullTerminatedString(Buff<byte> buffer)
+        {
+            Data = buffer.Ptr;
+        }
+
+        public NullTerminatedString(int addr)
+        {
+            Data = new Ptr<byte>(addr);
+        }
+    }
+}
