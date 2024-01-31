eb5a6ae4b001cea8c1ef44dd989e41001c64e1cd
Robert Baruch <robert.c.baruch@gmail.com>
2024-01-31 07:48:35 -0800

    Auto stash before merge of "improve_api" and "origin/improve_api"

---
+++ dergwasm_mod/Dergwasm/DergwasmSlots.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Threading.Tasks;
+using Elements.Core; // For UniLog
+using FrooxEngine;
+
+namespace Derg
+{
+    public class DergwasmSlots : IDergwasmSlots
+    {
+        public Slot DergwasmRoot { get; }
+        public Slot WasmBinarySlot { get; }
+
+        public Slot ConsoleSlot { get; }
+        public Slot FilesystemSlot { get; }
+
+        public bool Ready => DergwasmRoot != null && WasmBinarySlot != null;
+
+        public DergwasmSlots(IWorldServices worldServices)
+        {
+            DergwasmRoot = worldServices
+                .GetRootSlot()
+                .FindChild(s => s.Tag == "_dergwasm", maxDepth: 0);
+            if (DergwasmRoot == null)
+            {
+                DergwasmMachine.Msg(
+                    $"Couldn't find dergwasm slot with tag _dergwasm in world {worldServices.GetName()}"
+                );
+                return;
+            }
+
+            // We expect a slot with the tag _dergwasm_wasm_file to exist, and to have
+            // a StaticBinary component.
+            WasmBinarySlot = DergwasmRoot.FindChild(
+                s => s.Tag == "_dergwasm_wasm_file",
+                maxDepth: 0
+            );
+
+            ConsoleSlot = DergwasmRoot.FindChild(s => s.Tag == "_dergwasm_console_content");
+            FilesystemSlot = DergwasmRoot.FindChild(s => s.Tag == "_dergwasm_fs_root");
+        }
+
+        // Code adapted from BinaryExportable.Export.
+        public async Task<string> GatherWasmBinary(IWorldServices worldServices)
+        {
+            if (!Ready)
+            {
+                DergwasmMachine.Msg(
+                    $"World {worldServices.GetName()} slots are not set up to gather WASM binary"
+                );
+                return null;
+            }
+
+            StaticBinary binary = WasmBinarySlot.GetComponent<StaticBinary>();
+            if (binary == null)
+            {
+                DergwasmMachine.Msg(
+                    $"Couldn't access WASM StaticBinary component in world {worldServices.GetName()}"
+                );
+                return null;
+            }
+
+            FileMetadata metadata = WasmBinarySlot.GetComponent<FileMetadata>();
+            if (metadata != null)
+                metadata.IsProcessing.Value = true;
+            Uri url = binary.URL.Value;
+
+            worldServices.ToBackground();
+            string filename = await worldServices.GatherAssetFile(url, 100f);
+            UniLog.Log($"[Dergwasm] Gathered binary asset file {filename}");
+            worldServices.ToWorld();
+
+            if (metadata != null)
+                metadata.IsProcessing.Value = false;
+            return filename;
+        }
+    }
+}
---
+++ dergwasm_mod/Dergwasm/IDergwasmSlots.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using FrooxEngine;
+
+namespace Derg
+{
+    public interface IDergwasmSlots
+    {
+        Slot DergwasmRoot { get; }
+        Slot WasmBinarySlot { get; }
+
+        Slot ConsoleSlot { get; }
+        Slot FilesystemSlot { get; }
+
+        bool Ready { get; }
+
+        Task<string> GatherWasmBinary(IWorldServices worldServices);
+    }
+}
